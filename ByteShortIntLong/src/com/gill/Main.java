58e7bb175d3689e3db4e498cfe89473f8b191c09
rsgillit <rsgillit@gmail.com>
2019-11-19 15:08:00 -0600

    primitive datatypes

--- ByteShortIntLong/src/com/gill/Main.java
+++ ByteShortIntLong/src/com/gill/Main.java
@@ -3,6 +3,16 @@ package com.gill;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int myValue = 10000;
+
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+
+        System.out.println("myMinIntValue = " + myMinIntValue);
+        System.out.println("myMaxIntValue = " + myMaxIntValue);
+        System.out.println("Busted MAX value = " + (myMaxIntValue + 1));
+        System.out.println("Busted MIN value = " + (myMinIntValue - 1));
+
     }
 }
