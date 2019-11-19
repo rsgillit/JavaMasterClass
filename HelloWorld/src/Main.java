8b9e3e3fa83b30126e248667b28b53c52989aaea
rsgillit <rsgillit@gmail.com>
2019-11-19 14:49:34 -0600

    remaster java master class

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -3,6 +3,10 @@ public class Main {
     public static void main(String[] args) {
         //necessary ritual
         System.out.println("Hello World!");
+
+        int myFirstNumber = 5;
+        System.out.println("myFirstNumber = " + myFirstNumber);
+
     }
 }
 
