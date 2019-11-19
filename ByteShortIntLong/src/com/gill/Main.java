47ea4c606a4bf509837c06a36eb79b2c8e638a13
rsgillit <rsgillit@gmail.com>
2019-11-19 15:25:02 -0600

    primitive datatypes

--- ByteShortIntLong/src/com/gill/Main.java
+++ ByteShortIntLong/src/com/gill/Main.java
@@ -14,5 +14,28 @@ public class Main {
         System.out.println("Busted MAX value = " + (myMaxIntValue + 1));
         System.out.println("Busted MIN value = " + (myMinIntValue - 1));
 
+        int myMAXIntTest = 2_147_483_647;
+
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+
+        System.out.println("Byte Minimum value = " + myMinByteValue);
+        System.out.println("Byte Maximum value = " + myMaxByteValue);
+
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+
+        System.out.println("Short Minimum value = " + myMinShortValue);
+        System.out.println("Short Maximum value = " + myMaxShortValue);
+
+        long myLongValue = 100L;
+
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+
+        System.out.println("Long Minimum value = " + myMinLongValue);
+        System.out.println("Long Maximum value = " + myMaxLongValue);
+
+
     }
 }
