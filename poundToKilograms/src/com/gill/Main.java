ada20285e558e7442a751b3804a306ca66eb5af8
rsgillit <rsgillit@gmail.com>
2019-11-20 08:45:25 -0600

    first commit

---
+++ poundToKilograms/src/com/gill/Main.java
@@ -0,0 +1,14 @@
+package com.gill;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        double weightlb = 200d;
+
+        double weightKg = weightlb * 0.45359237d;
+
+        System.out.println("Converted Kilograms = " + weightKg);
+
+    }
+}
