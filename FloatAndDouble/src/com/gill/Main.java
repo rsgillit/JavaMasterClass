5924f370c93a0760317775de62355c3d8209852a
rsgillit <rsgillit@gmail.com>
2019-11-20 08:30:24 -0600

    float primitive types

---
+++ FloatAndDouble/src/com/gill/Main.java
@@ -0,0 +1,26 @@
+package com.gill;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+        System.out.println("Float minimum value = " + myMinFloatValue);
+        System.out.println("Float maximum value = " + myMaxFloatValue);
+
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+        System.out.println("Double minimum value = " + myMinDoubleValue);
+        System.out.println("Double maximum value = " + myMaxDoubleValue);
+
+        int myIntValue = 5;
+        float myFloatValue = (float)5.212; //or f at the end
+        double myDoubleValue = 5.25111;
+
+        System.out.println("myFloatValue = " + myFloatValue);
+
+
+
+    }
+}
