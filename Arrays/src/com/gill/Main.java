30a7e53b7acf471530b1d91614a7e9b75ccd83bd
rsgillit <rsgillit@gmail.com>
2019-11-29 15:40:07 -0600

    arrays

---
+++ Arrays/src/com/gill/Main.java
@@ -0,0 +1,13 @@
+package com.gill;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int[] myIntArray = new int[10];
+	    myIntArray[5] = 50;
+	    for(int i=0;i<10;i++){
+            System.out.println(i + ":" + myIntArray[i]);
+        }
+
+    }
+}
