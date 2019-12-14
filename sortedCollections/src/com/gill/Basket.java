ea64a5cce7ea1dc3e6ba9a26f529cde82c39158e
rsgillit <rsgillit@gmail.com>
2019-12-14 11:21:03 -0600

    sortedCollections

---
+++ sortedCollections/src/com/gill/Basket.java
@@ -0,0 +1,42 @@
+package com.gill;
+
+import com.sun.jdi.Value;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Basket {
+    private final String name;
+    private final Map<StockItem, Integer> list;
+
+    public Basket(String name) {
+        this.name = name;
+        this.list = new TreeMap<>();
+    }
+
+    public int addToBasket(StockItem item, int quantity) {
+        if(item != null && quantity>0){
+            int inBasket = list.getOrDefault(item, 0);
+            list.put(item, inBasket + quantity);
+            return inBasket;
+        }
+        return 0;
+    }
+
+    public Map<StockItem, Integer> item() {
+        return Collections.unmodifiableMap(list);
+    }
+
+    @Override
+    public String toString() {
+        String s = "\n shopping basket " + name + " contains " + list.size() + " items\n";
+        double totalCost = 0.0;
+        for(Map.Entry<StockItem,Integer> item : list.entrySet()) {
+           s = s + item.getKey() + ". " + item.getValue() + " purchased\n";
+           totalCost += item.getKey().getPrice() * item.getValue();
+        }
+        return s + "Total cost " + totalCost;
+    }
+}
